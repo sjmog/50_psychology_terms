284c5862f0e07648f01ace64b85cf30c21957064
Sam Morgan <s_morgan@me.com>
2016-04-16 14:44:51 +0100

    (WIP) Basic layout with Navigation structure

--- Gemfile
+++ Gemfile
@@ -13,3 +13,8 @@ gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
 gem 'middleman-compass', '>= 4.0.0'
 gem 'middleman-deploy', '~> 2.0.0.pre.alpha'
+
+# SCSS Gems
+gem 'bourbon'
+gem 'neat'
+gem 'bitters'
--- config.rb
+++ config.rb
@@ -36,18 +36,10 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
 end
 
 activate :deploy do |deploy|
   deploy.deploy_method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
